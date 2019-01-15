6a73c079fc76a520e9f86e76e6b5f9fd10af8724
yufrances90 <yufrances90@gmail.com>
2019-01-14 22:52:29 -0800

    feat: Got user input for deck title

--- udacicards/pages/PNewDeck.js
+++ udacicards/pages/PNewDeck.js
@@ -14,7 +14,30 @@ import DismissKeyboard from 'dismissKeyboard';
 import globalStyles from '../styles/styles';
 
 export default class PNewDeck extends Component {
+
+    state = {
+        deckTitle: ''
+    }
+
+    handleChange(value) {
+        this.setState({
+            deckTitle: value
+        });
+    }
+
+    handleClick(event) {
+        
+        alert(this.state.deckTitle);
+
+        this.setState({
+            deckTitle: ''
+        })
+    }
+
     render() {
+
+        const { deckTitle } = this.state;
+
         return (
             <TouchableWithoutFeedback
                 onPress={()=>{DismissKeyboard()}} 
@@ -22,10 +45,15 @@ export default class PNewDeck extends Component {
                 <View style={globalStyles.centeredContainer}>
                     <Text style={styles.title}>What is the title of your new deck?</Text>
                     <TextInput 
-                    placeholder="Enter New Deck Title Here..."
+                        placeholder="Enter New Deck Title Here..."
+                        value={deckTitle}
+                        onChangeText={this.handleChange.bind(this)}
+                        autoCorrect={false}
+                        autoCapitalize={"none"}
                     />
                     <TouchableOpacity
                         style={globalStyles.submitBtn}
+                        onPress={this.handleClick.bind(this)}
                     >
                         <Text style={globalStyles.submitBtnText}>Submit</Text>
                     </TouchableOpacity>
